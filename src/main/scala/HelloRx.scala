b6314bd907d88f4c27fb5cfbc2deeb24721f2d89
manojo <manojo10386@gmail.com>
2014-05-01 16:02:42 +0200

    observable from infinite stream, using the new apply function

--- src/main/scala/HelloRx.scala
+++ src/main/scala/HelloRx.scala
@@ -7,6 +7,8 @@ import scala.concurrent.duration.DurationLong
 import scala.language.postfixOps
 import scala.language.implicitConversions
 
+import scala.collection.Iterator
+
 trait SimpleObservables {
 
   val fromIterable = Observable.from(List(1,2,3,4,5))
@@ -18,11 +20,24 @@ trait SimpleObservables {
 
 trait TimedObservables {
 
-  val simpleInterval: Observable[Long] = Observable.interval(1 seconds)
+  val simpleInterval: Observable[Long] = Observable.interval(1 seconds).doOnNext(x => println("in simple"))
 
-  val interval2: Observable[Long] = Observable.interval(4 seconds)
+  val interval2: Observable[Long] = Observable.interval(4 seconds).doOnNext(x => println("in interval2"))
 
-  val simpleMapped: Observable[Long] = interval2.map { i => Observable.interval(1 seconds).map{x => (x + 1) * i}}.switch
+  val simpleMapped: Observable[Long] = interval2.map { i => Observable.interval(1 seconds).map{x => (x + 1) * i}}.switch.doOnNext(x => println("in simpleMapped"))
+
+  //inspired by stuff in RxScalademo:
+  // https://github.com/Netflix/RxJava/blob/master/language-adaptors/rxjava-scala/src/examples/scala/rx/lang/scala/examples/RxScalaDemo.scala
+  val simpleIterator: Observable[Int] = Observable { subscriber =>
+    var i = 0
+    while(!subscriber.isUnsubscribed) {
+      subscriber.onNext{ println("whappen deh: "+ i); i }
+      i += 1
+    }
+
+    if(subscriber.isUnsubscribed){ subscriber.onCompleted() }
+  }
+
 
 }
 
@@ -30,10 +45,15 @@ object HelloRx extends App with TimedObservables {
 
   println("Whappen! Greetings Lion!")
 
+  //simpleIterator.take(10).toBlockingObservable.foreach { i => {println(i)} }
+  simpleIterator.take(10).toBlockingObservable.foreach { i => println(i) }
+
 
 //  fromIterable.subscribe{ i => println(i) }
 //  println("watch out!")
 
-  simpleMapped.toBlockingObservable.foreach { i => println(i) }
+//  simpleMapped.take(10).toBlockingObservable.foreach { i => println(i) }
+
+
 
 }
